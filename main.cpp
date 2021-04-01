b3fa7c39e00d48b587c7ad2a437f82d215b4e550
AndreyYavorsky <80091510+AndreyYavorsky@users.noreply.github.com>
2021-04-01 11:43:12 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <cstdio>
 
 extern "C"{
 	extern int sum_squar(int** a , int num );
@@ -16,64 +16,3 @@ int main(void)
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <stdio.h>
-
-// int sum_quad(int* ptr_arr, int num);
-// // int sum_quad (int (*ptr_arr)[5], int n1, int n2 )
-// // {
-// // 	int res = 0;
-// // 	for (int i = 0; i < n1; ++i)
-// // 	{
-// // 		for (int j = 0; j < n2; ++j)
-// // 		{
-// // 			res += ptr_arr[i][j] * ptr_arr[i][j];
-// // 		}
-// // 	}
-// // 	return res;
-// // }
-
-
-// int main(void)
-// {
-
-// 	// int array[5][5] ={{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1}};
-// 	int array[] = {2,2,2,2,2};
-
-// 	// int arr[] = {1,2,3,4};
-// 	// printf("%d\n",0[arr]);
-
-// 	int sum = sum_quad(array, 5);
-
-// 	printf("%d\n", sum );
-
-
-// 	return 0;
-// }
